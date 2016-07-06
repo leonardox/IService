e700ed84bf7b1683c988b21daff0cd28098c4eaf
Leonardo.Cordeiro <leo.cc14@gmail.com>
2016-07-05 22:14:08 -0300

    Adicionando novos atributos e construtor default

--- IService_Server/IService_Server/iservice/models.py
+++ IService_Server/IService_Server/iservice/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class IserviceUser(User):
@@ -9,4 +10,23 @@ class IserviceUser(User):
     This class represents a system user.
     """
 
-    picture = models.CharField(max_length=150)
+    name = models.CharField(max_length=100)
+    picture = models.CharField(null=True, max_length=150)
+    phone = PhoneNumberField(null=True)
+
+    @staticmethod
+    def create_user(**kwargs):
+        """
+        This fuction inserts an user on database.
+        :param kwargs:
+        :return: User instance.
+        """
+        user = IserviceUser()
+        user.name = kwargs['name']
+        user.email = kwargs['email']
+        user.phone = kwargs['phone']
+        user.picture = kwargs['picture']
+        user.set_password(kwargs['password'])
+        user.username = user.email
+        user.save()
+        return user
